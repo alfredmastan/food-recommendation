bef2847ac9ee2827e79a636c5cb65d4167e5868b
alfredmastan <alfredmastan@outlook.com>
2025-09-21 19:21:16 -0700

    build: Switch to EC2 for service

--- service/model_service.py
+++ service/model_service.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Query
 import uvicorn
 import mlflow
-from mangum import Mangum
 import json
 
 # Preparation for Service ============================================================
@@ -11,39 +10,24 @@ model = mlflow.pyfunc.load_model("service/model/")
 # Create FastAPI app
 app = FastAPI(title="Food-Recipe_Model-API")
 
-# Handler for AWS Lambda
-handler = Mangum(app)
-
 # API Calls ============================================================
 @app.get("/")
 async def read_root():
-    return {"Welcome": "Welcome to the FastAPI on Lambda"}
+    return {"message": "Healthy!"}
 
-# @app.post("/recommend/")
-# async def recommend_search(query: list[str] = Query(default=[],
-#                                               description="List of ingredients to search for",
-#                                               examples=["chicken", "rice"])):
-#     """
-#     Search for recipes based on a query string.
-#     """
+@app.post("/recommend/")
+async def recommend_search(query: list[str] = Query(default=[],
+                                              description="List of ingredients to search for",
+                                              examples=["chicken", "rice"])):
+    """
+    Search for recipes based on a query string.
+    """
 
-#     if not query:
-#         return json.dumps([])
+    if not query:
+        return json.dumps([])
     
-#     rec_idx = model.predict(query)
-#     return json.dumps(rec_idx.tolist())
+    rec_idx = model.predict(query)
+    return json.dumps(rec_idx.tolist())
 
-# if __name__ == "__main__":
-#     uvicorn.run(app="model_service:app", host="0.0.0.0", port=8000)
-
-# curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"resource": "/", "path": "/", "httpMethod": "GET", "requestContext": {}, "multiValueQueryStringParameters": null}'
-
-# curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" \
-# -H "Content-Type: application/json" \
-# -d '{
-#   "resource": "/recommend",
-#   "path": "/recommend",
-#   "httpMethod": "POST",
-#   "requestContext": {},
-#   "queryStringParameters": {"query": "chicken"},
-# }'
+if __name__ == "__main__":
+    uvicorn.run(app="model_service:app", host="0.0.0.0", port=8000)
